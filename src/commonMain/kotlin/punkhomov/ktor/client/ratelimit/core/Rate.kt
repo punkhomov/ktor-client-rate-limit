9fdce1323ddc8d9330e7d61c7e6e6b24d9efbf44
punkhomov <punkhomov@gmail.com>
2024-02-10 17:53:25 +0400

    validation rate init parameters

--- src/commonMain/kotlin/punkhomov/ktor/client/ratelimit/core/Rate.kt
+++ src/commonMain/kotlin/punkhomov/ktor/client/ratelimit/core/Rate.kt
@@ -19,4 +19,11 @@ data class Rate(
      * @see periodTime
      */
     val periodUnit: DurationUnit,
-)
+) {
+    init {
+        require(permits > 0) { "'permits' must be a positive number." }
+        require(periodTime > 0) { "'periodTime' must be a positive number." }
+        require(periodUnit != DurationUnit.NANOSECONDS) { "Nanoseconds are too small for delays." }
+        require(periodUnit != DurationUnit.MICROSECONDS) { "Microseconds are too small for delays." }
+    }
+}
